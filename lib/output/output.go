336d03bf33887073799bd0681f54d9250d75e506
Marco Franssen <marco.franssen@philips.com>
2020-06-19 20:59:33 +0200

    Fix html character encoding like & etc.

--- lib/output/output.go
+++ lib/output/output.go
@@ -2,16 +2,19 @@ package output
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
 )
 
 // PrintJSON prints the json using indentation using the given writer
 func PrintJSON(w io.Writer, data interface{}) error {
-	json, err := json.MarshalIndent(data, "", "  ")
+	enc := json.NewEncoder(w)
+	enc.SetEscapeHTML(false)
+	enc.SetIndent("", "  ")
+
+	err := enc.Encode(data)
 	if err != nil {
 		return err
 	}
-	fmt.Fprintf(w, "%s\n", json)
+
 	return nil
 }
